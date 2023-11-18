0c8a63574a54b038494f7d7c0f386de5bb47fd59
SaraBoricic <149419014+SaraBoricic@users.noreply.github.com>
2023-11-18 13:10:40 +0100

    0703

---
+++ 01-simple/Models2/Actor.cs
@@ -0,0 +1,11 @@
+namespace DSS.Models2
+{
+    public class Actor:Man
+    {
+        public string ImdbMark = 5;
+
+        public string NumberOfOscars = 0;
+
+        public double NumberOfBaftas = 0;
+    }
+}
---
+++ 01-simple/Models2/Address.cs
@@ -0,0 +1,13 @@
+namespace DSS.Models2
+{
+    public class Address
+    {
+        public string Streat = "";
+
+        public string City = "";
+
+        public string PostalCode = "";
+
+        public string State = "";
+    }
+}
